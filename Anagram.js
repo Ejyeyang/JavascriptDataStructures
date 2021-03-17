498f8bc0ceecc1cb7736656575361384ec741e77
Edward Yang <eyang@sefnco.com>
2021-03-17 12:25:07 -0700

    anagram test

--- Anagram.js
+++ Anagram.js
@@ -46,3 +46,26 @@ anagramTwo(one, two){
     }
     return true;
 }
+
+anagramThree(one, two){
+    if(one.length != two.length){
+        return false;
+    }
+
+    const lookUp = {}
+
+    for(let i = 0; i < one.length; i++){
+        let letter = one[i];
+        lookUp[letter] ? lookUp[letter] += 1 : lookUp[letter] = 1;
+    }
+
+    for(let i = 0; i < two.length; i++){
+        let letter = two[i];
+        if(!lookUp[letter]){
+            return false;
+        } else {
+            lookUp[letter] -= 1; 
+        }
+    }
+    return true;
+}
