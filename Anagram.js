8efbcf822aac34312783c330682b28063e500a17
Edward Yang <ejyeyang@gmail.com>
2021-03-18 13:12:15 -0700

    testing

--- Anagram.js
+++ Anagram.js
@@ -116,3 +116,26 @@ anagramsAgain(one, two){
 
     }
 }
+
+anagramAgain(one, two){
+    if(one.length != two.length){
+        return false;
+    }
+
+    const lookUp = {};
+
+    for(let i = 0; i < one.length; i++){
+        let letter = one[i];
+        lookUp[letter] ? lookUp += 1 : lookUp[letter] = 1; 
+    }
+
+    for(let i = 0; i < two.length; i++){
+        let letter = two[i];
+        if(!lookUp[letter]){
+            return false;
+        } else {
+            lookUp[letter] -= 1;
+        }
+    }
+    return true;
+}
