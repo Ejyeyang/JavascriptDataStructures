b5f1d004ed3fbd1eeac166b55f6ceba88b5ff43b
Edward Yang <ejyeyang@gmail.com>
2021-03-18 13:37:00 -0700

    testing

--- Anagram.js
+++ Anagram.js
@@ -26,3 +26,26 @@ anagram(one, two){
 
     return true;
 }
+
+anagramTwo(one, two){
+    if(one.length != two.length){
+        return false;
+    }
+
+    const lookUp = {};
+
+    for(let i = 0; i < one.length; i++){
+        let letter = one[i];
+        lookUp[letter] ? lookUp[letter] += 1 : lookUp[letter] = 1; 
+    }
+
+    for(let i = 0; i < two.length; i++){
+        let letter = two[i];
+        if(!lookUp[letter]){
+            return false;
+        } else {
+            lookUp[letter] -= 1; 
+        }
+    }
+    return false;
+}
