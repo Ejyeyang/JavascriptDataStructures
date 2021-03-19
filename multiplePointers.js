fa4b17f0b1f82c195daddfbc17bd96ad5f1d33bc
Edward Yang <ejyeyang@gmail.com>
2021-03-19 12:50:33 -0700

    multiple pointers practice

---
+++ multiplePointers.js
@@ -0,0 +1,25 @@
+arrOne = [-3, -2 , -1, 0, 1, 2, 3]; // returns [-3, 3]
+arrTwo = [-2, 0, 1, 3]; //should return undefined
+arrThree = [1, 2, 3]; //should return undefined
+
+//write a function called sumZero which accepts a sorted array 
+//of integers, the function should find the first the first pair
+//where the sum is 0. Return an array that includes both the values
+//that sum to zero or return undefined if the pair does not exist
+//in the array. 
+
+sumZero(arr){
+    let left = 0; 
+    let right = arr.length -1; 
+    //until reaches half way mark of array
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        } else if(sum > 0){
+            right--;
+        } else {
+            left--;
+        }
+    }
+}
