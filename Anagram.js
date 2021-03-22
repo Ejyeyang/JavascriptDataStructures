e83040b00cece00c0eb1c9053a5e2b4bda247415
Edward Yang <ejyeyang@gmail.com>
2021-03-22 08:09:21 -0700

    anagram

--- Anagram.js
+++ Anagram.js
@@ -3,29 +3,7 @@ validAnagram('aaz', 'zza');
 validAnagram('anagram', 'nagaram');
 validAnagram('rat', 'car');
 
-anagramAgain(one, two){
-    if(one.length != two.length){
-        return false;
-    }
-
-    const lookUp = {};
-    for(let i = 0; i < one.length; i++){
-        let letter = one[i];
-        lookUp[letter] ? lookUp[letter] += 1 : lookUp[letter] = 1;
-    }
-
-    for(let i = 0; i < two.length; i++){
-        let letter = two[i];
-        if(!lookUp[letter]){
-            return false;
-        } else{
-            lookUp[letter] -= 1; 
-        }
-    }
-    return true;
-}
-
-anagram(one, two){
+function anagramPractice(one, two){
     if(one.length != two.length){
         return false;
     }
@@ -45,52 +23,5 @@ anagram(one, two){
             lookUp[letter] -= 1; 
         }
     }
-
-    return true;
-}
-
-anagramTwo(one, two){
-    if(one.length != two.length){
-        return false;
-    }
-
-    const lookUp = {};
-
-    for(let i = 0; i < one.length; i++){
-        let letter = one[i];
-        lookUp[letter] ? lookUp[letter] += 1 : lookUp[letter] = 1; 
-    }
-
-    for(let i = 0; i < two.length; i++){
-        let letter = two[i];
-        if(!lookUp[letter]){
-            return false;
-        } else {
-            lookUp[letter] -= 1; 
-        }
-    }
-    return false;
-}
-
-anagramThree(one, two){
-    if(one.length != two.length){
-        return false;
-    }
-
-    const lookUp = {};
-
-    for(let i = 0; i < one.length; i++){
-        let letter = one[i];
-        lookUp[letter] ? lookUp[letter] += 1 : lookUp[letter] = 1; 
-    }
-
-    for(let i = 0; i < two.length; i++){
-        let letter = two[i];
-        if(!lookUp[letter]){
-            return false;
-        } else {
-            lookUp[letter] -= 1; 
-        }
-    }
-    return true;
+    return true; 
 }
