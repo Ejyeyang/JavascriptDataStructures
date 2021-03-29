136a2e84be2f2f6cc8b553234495ad3ee71a065a
Edward Yang <ejyeyang@gmail.com>
2021-03-29 13:05:39 -0700

    linear search function 0ofN

--- searchingDataStructures/linearSearch.js
+++ searchingDataStructures/linearSearch.js
@@ -5,3 +5,18 @@
 'New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands',
 'Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota',
 'Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+/**
+ * linear search is when you search
+ * from beginning to end. 
+ */
+
+//returns index of target 
+function linearSearch(arr, target){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === target){
+            return i;
+        }
+    }
+    return -1; 
+}
