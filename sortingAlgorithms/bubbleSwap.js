25ece64a88f3b9a1fef8ac0553b350c91c16d276
Edward Yang <ejyeyang@gmail.com>
2021-04-01 14:33:21 -0700

    different ways to swap

---
+++ sortingAlgorithms/bubbleSwap.js
@@ -0,0 +1,11 @@
+
+function swap(arr, ind1, ind2){
+    var temp = arr[ind1];
+    arr[ind1] = arr[ind2]; 
+    arr[ind2] = temp; 
+}
+
+//same as above
+const swap2 = (arr, ind1, ind2) => {
+    [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]]; 
+}
