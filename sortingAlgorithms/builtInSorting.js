86205c0434801f783cbd4c1aef4a6e38bbb75f6b
Edward Yang <ejyeyang@gmail.com>
2021-04-01 14:09:59 -0700

    built in javascript sorting

--- sortingAlgorithms/builtInSorting.js
+++ sortingAlgorithms/builtInSorting.js
@@ -4,8 +4,14 @@ arr.sort();
 //for ints it will sort my unicode so not by values
 arr.sort();
 
-//this sorts ints from small to large
+//this sorts ints from small to large works for strings too
 function numberCompare(num1, num2){
     return num1 - num2; 
 }
 arr.sort(numberCompare); 
+
+//this sorts ints from large to small works for strings too. 
+function numCompare(num1, num2){
+    return num2 - num1;
+}
+arr.sort(numCompare); 
