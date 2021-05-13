0ed444c628537def236bdd58b1d6b1323bb484e3
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-05-12 21:11:55 -0700

    divide and conquer

--- problemSolvingPatterns/divideAndConquer.js
+++ problemSolvingPatterns/divideAndConquer.js
@@ -20,7 +20,22 @@ function searchAndDestroy(array, target){
     }
     return -1; 
 }
+function searchAndDestroy(array, target){
+    let left = 0; 
+    let right = array.length -1; 
 
+    while(left <= right){
+        let middle = Math.floor((left + right) / 2); 
+        if(arr[middle] > target){
+            right = middle - 1; 
+        } else if(ar[middle] < target){
+            left = middle + 1;
+        } else {
+            return middle; 
+        }
+    }
+    return -1; 
+}
 
 function searchAndDestroy(array, target){
     let left = 0; 
