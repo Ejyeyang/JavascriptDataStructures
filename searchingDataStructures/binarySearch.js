e4d5c6af6f5386ce9245a754276917f45cda8da4
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-05-12 21:09:46 -0700

    binary search

--- searchingDataStructures/binarySearch.js
+++ searchingDataStructures/binarySearch.js
@@ -61,3 +61,19 @@ function binarySearch(arr, target){
     return -1; 
 }
 
+function binarySearch(arr, target){
+    let left = 0; 
+    let right = arr.length - 1; 
+    
+    while(left <= right){
+        let middle = Math.floor((left + right) / 2); 
+        if(target < arr[middle]){
+            right = middle - 1; 
+        } else if(target > arr[middle]){
+            left = middle + 1; 
+        } else {
+            return middle; 
+        }
+    }
+    return -1; 
+}
