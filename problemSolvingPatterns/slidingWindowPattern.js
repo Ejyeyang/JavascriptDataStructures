3f98e99670b3b387bcf9128d164681b8b67df2bb
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-05-12 21:11:04 -0700

    slidingWindowPattern

--- problemSolvingPatterns/slidingWindowPattern.js
+++ problemSolvingPatterns/slidingWindowPattern.js
@@ -27,6 +27,26 @@ function maxSub(arr, num){
     return max; 
 }
 
+function maxSub(arr, num){
+    if(arr < num){
+        return null;
+    }
+
+    let max = 0; 
+    let temp = 0; 
+
+    for(let i = 0; i < num; i++){
+        max += arr[i];
+    }
+
+    temp = max; 
+    for(let i = num; i < arr.length; i++){
+        temp = temp - arr[i - num] + arr[i];
+        max = Math.max(max, temp);
+    }
+
+    return max; 
+}
 
 function maxSubArraySum(arr, num){
     if(arr.length < num){
