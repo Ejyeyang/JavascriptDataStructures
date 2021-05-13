745f0d2a4e25fca3b103a0db65f51ce36a60998e
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-05-12 21:08:52 -0700

    bubblesort

--- sortingAlgorithms/bubbleSwap.js
+++ sortingAlgorithms/bubbleSwap.js
@@ -9,3 +9,13 @@ function swap(arr, ind1, ind2){
 const swap2 = (arr, ind1, ind2) => {
     [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]]; 
 }
+
+const swap2 = (arr, ind1, ind2) => {
+    [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]]; 
+}
+
+function swap(arr, ind1, ind2){
+    var temp = arr[ind1];
+    arr[ind1] = arr[ind2]; 
+    arr[ind2] = temp; 
+}
